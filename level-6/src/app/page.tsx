6f905a28bbe16fd2733c14a89231bc226b2fa848
Sohan Samanta <sohansamanta18@gmail.com>
2026-08-13 02:37:02 +0530

    done

---
+++ level-6/src/app/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div >
+
+        <h1>Welcome to Level - 6</h1>
+
+        <p>This is a API Routes Page</p>
+
+
+      </div>
+    </main>
+  )
+}
+
+export default page
