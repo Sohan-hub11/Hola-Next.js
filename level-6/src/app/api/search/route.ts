35ce252a062bc3f406e5adddca995f2071360733
Sohan Samanta <sohansamanta18@gmail.com>
2026-08-13 03:12:34 +0530

    done

---
+++ level-6/src/app/api/search/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request:NextRequest) {
+    const query= request.nextUrl.searchParams.get("q")
+    return NextResponse.json({query})
+}
--- level-6/src/app/api/user/route.ts
+++ level-6/src/app/api/user/route.ts
@@ -1,9 +1,20 @@
 //NextRequest & NextResponse
 
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
+
+//GET Request.
 export async function GET(){
   return NextResponse.json({
-    
+    "name":"Sohan",
+    "age":22
+  })
+}
+
+//POST Request.
+export async function POST(req:NextRequest) {
+  const {name, age} =await req.json();
+  return NextResponse.json({
+    name,age
   })
 }
