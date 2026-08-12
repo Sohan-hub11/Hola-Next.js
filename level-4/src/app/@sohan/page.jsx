1c681927906fd18e8b3b7756d871f9a5d375a69d
Sohan Samanta <sohansamanta18@gmail.com>
2026-08-12 16:27:00 +0530

    done

--- level-4/src/app/@sohan/page.jsx
+++ level-4/src/app/@sohan/page.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        Team Page
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"> 
+        Sohan Page
       </div>
     </main>
   )
 }
 
-export default page
+export default page;
--- level-4/src/app/layout.jsx
+++ level-4/src/app/layout.jsx
@@ -11,15 +11,16 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export default function RootLayout({ children, team }) {
+export default function RootLayout({ children, team, sohan }) {
   return (
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="w-screen h-screen flex">
-        <div className="w-[50%] bg-amber-950">{children}</div>
-        <div className="w-[50%] bg-blue-950">{team}</div>
+        <div className="w-[30%] bg-orange-800">{sohan}</div>
+        <div className="w-[40%] bg-amber-950">{children}</div>
+        <div className="w-[30%] bg-blue-950">{team}</div>
       </body>
     </html>
   );
