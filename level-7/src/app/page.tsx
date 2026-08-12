3789e13867461f8a45bc2cd76cbfe55718985a11
Sohan Samanta <sohansamanta18@gmail.com>
2026-08-13 03:15:05 +0530

    done

---
+++ level-7/src/app/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div >
+
+        <h1>Welcome to Level - 7</h1>
+
+        <p>This is a Data Fetching Page</p>
+
+
+      </div>
+    </main>
+  )
+}
+
+export default page
