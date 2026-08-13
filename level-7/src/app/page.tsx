59f19e5c95f3fa4d65aaf930070b61243b0dc9e9
Sohan Samanta <sohansamanta18@gmail.com>
2026-08-14 02:52:37 +0530

    done

--- level-7/src/app/page.tsx
+++ level-7/src/app/page.tsx
@@ -1,6 +1,41 @@
-import React from 'react'
+//Note- SSR, SSG and ISR works only on server component.
 
-function page() {
+'use client'
+import React, { useEffect } from 'react'
+
+function Page() {
+
+  //SSR
+  // const response = await fetch('http://localhost:3000/api/user', {
+  //   cache: 'no-store'
+  // })
+  // const data = await response.json()
+  // console.log(data)
+
+  //SSG
+  // const response = await fetch('http://localhost:3000/api/user', {
+  //   cache: 'force-cache'
+  // })
+  // const data = await response.json()
+  // console.log(data)
+
+  //ISR
+  // const response = await fetch('http://localhost:3000/api/user', {
+  //   next:{revalidate:5}
+  // })
+  // const data = await response.json()
+  // console.log(data)
+
+
+  const handleApi = async () => {
+    const response = await fetch('/api/user')
+    const data = await response.json()
+    console.log(data)
+  }
+  useEffect(() => {
+    handleApi()
+  }, [])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div >
@@ -15,4 +50,4 @@ function page() {
   )
 }
 
-export default page
+export default Page
