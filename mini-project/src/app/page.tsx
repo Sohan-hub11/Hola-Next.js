841f2cf9d2dfba17322eae29fb718080941c9443
Sohan Samanta <sohansamanta18@gmail.com>
2026-08-16 14:42:00 +0530

    done

--- mini-project/src/app/page.tsx
+++ mini-project/src/app/page.tsx
@@ -2,7 +2,10 @@
 export default function Home() {
   return (
     <main className="flex w-screen h-screen flex-col items-center justify-between px-24 py-6">
-
+      <div className="input-area fixed bottom-6 px-24 py-2 flex items-center justify-between w-full">
+        <input className="border p-2 rounded-md w-[80%] outline-none" placeholder="Type here...."/>
+        <button className="cursor-pointer bg-neutral-700 text-white p-2 rounded-md ml-4 w-[18%]">Send</button>
+      </div>
     </main>
   );
 }
